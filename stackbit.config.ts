651ae846765f91664418673ae6999cb274483a15
Meilech Wieder <61207623+meilechwieder@users.noreply.github.com>
2025-03-04 17:07:30 -0500

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,5 +1,5 @@
 import { defineStackbitConfig } from '@stackbit/types'; 
-export default defineStackbitConfig({ "stackbitVersion": "~0.6.0", "nodeVersion": "18", "ssgName": "custom", "contentSources": [], "postInstallCommand": "npm i --no-save @stackbit/types" ,
+export default defineStackbitConfig({ "stackbitVersion": "~0.6.0", "nodeVersion": "18", "ssgName": "custom", "postInstallCommand": "npm i --no-save @stackbit/types" ,
 contentSources: [ new GitContentSource({ rootPath: __dirname, contentDirs: ["content"], 
 models: [ { name: "Page", // Define the model as a page model 
 type: "page", urlPath: "/{slug}", filePath: "content/pages/{slug}.json", fields: [{ name: "title", type: "string", required: true }] } ], }) ] })
