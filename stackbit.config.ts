b9be5cfcf5306f88c11ecafa188d2df122f422f1
Meilech Wieder <61207623+meilechwieder@users.noreply.github.com>
2025-03-07 14:17:17 -0500

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,5 +1,28 @@
-import { defineStackbitConfig } from '@stackbit/types'; 
-export default defineStackbitConfig({ "stackbitVersion": "~0.6.0", "nodeVersion": "18", "ssgName": "custom", "postInstallCommand": "npm i --no-save @stackbit/types" ,
-contentSources: [ new GitContentSource({ rootPath: __dirname, contentDirs: ["content"], 
-models: [ { name: "Page", // Define the model as a page model 
-type: "page", urlPath: "/{slug}", filePath: "content/pages/{slug}.json", fields: [{ name: "title", type: "string", required: true }] } ], }) ] })
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "public",
+        uploadDir: "images",
+        publicPath: "/"
+      }
+    })
+  ]
+});
